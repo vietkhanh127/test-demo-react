12cd23aee3ce4077eb1b9bf3aea79354315b71c7
Viet Khanh <vietkhanh93@gmail.com>
2024-08-25 08:45:01 +0700

    video17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'eric',
+        address: 'hoidanit',
+        age: 16
+
+    }
     render() {
         return (
             <div>
-                My first component
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
         )
     }
